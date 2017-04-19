6a4d4c7615d1d717a33d74846479f2c141838bf5
Steven Xie <Steven Xie>
2017-04-19 04:50:51 -0400

    k so i made this new thing called packaged fonts and its actually really weird and i need to make my textfield class more readable n stuff

--- clientapp3/CustomLabel.swift
+++ clientapp3/CustomLabel.swift
@@ -10,18 +10,25 @@ import UIKit
 
 class CustomLabel: UILabel{
     
-    enum FontStyle {
-        case header1, header2, header3, body1
-    }
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    init(text: String, fontSize: CGFloat, fontWeight: String, fontColor: UIColor, lineWidthLimit: CGFloat? = nil) {
+    convenience init(text: String, fontSize: CGFloat, fontWeight: String, fontColor: UIColor, lineWidthLimit: CGFloat? = nil) {
+        if let font = UIFont(name: "AvenirNext-" + fontWeight, size: fontSize){
+            self.init(text: text, font: font, fontColor: fontColor, lineWidthLimit: lineWidthLimit)
+        }
+        else{
+            self.init(frame: CGRect.zero)
+        }
+    }
+    
+    init(text: String, font: UIFont, fontColor: UIColor, lineWidthLimit: CGFloat? = nil){
         super.init(frame: CGRect.zero)
         super.text = text
-        super.font = UIFont(name: "AvenirNext-" + fontWeight, size: fontSize)
+        super.font = font
         super.textColor = fontColor
         
         if let maxWidth = lineWidthLimit{
@@ -30,41 +37,22 @@ class CustomLabel: UILabel{
             super.frame.size.width = maxWidth
         }
         
-        super.sizeToFit()
+        sizeToFit()
+    }
+    
+    convenience init(text: String, style: FontStyle, lineWidthLimit: CGFloat? = nil) {
+        if let packagedFont = constructFont(style){
+            let font = packagedFont["font"] as! UIFont
+            let color = packagedFont["color"] as! UIColor
+            self.init(text: text, font: font, fontColor: color, lineWidthLimit: lineWidthLimit)
+        }
+        else{
+            self.init(frame: CGRect.zero)
+        }
     }
     
     func shiftOriginFromCenter(byFactor offsetFactor: CGFloat, parent: UIView){
         self.frame.origin.x = parent.frame.width/2 + (offsetFactor * parent.frame.width)
-    }
-    
-    convenience init(text: String, style: FontStyle, lineWidthLimit: CGFloat? = nil) {
-        var fontSize: CGFloat
-        var fontWeight: String
-        var fontColor: UIColor
-        
-        switch style{
-        case .header1:
-            fontSize = FontSize.header1
-            fontWeight = "Medium"
-            fontColor = FontColor.headerGrey1
-            
-        case .header2:
-            fontSize = FontSize.header2
-            fontWeight = "Medium"
-            fontColor = FontColor.headerGrey2
-            
-        case .header3:
-            fontSize = FontSize.header2
-            fontWeight = "Medium"
-            fontColor = FontColor.headerGrey3
-            
-        case .body1:
-            fontSize = FontSize.body1
-            fontWeight = "Regular"
-            fontColor = FontColor.bodyGrey1
-        }
-        
-        self.init(text: text, fontSize: fontSize, fontWeight: fontWeight, fontColor: fontColor, lineWidthLimit: lineWidthLimit)
     }
     
     override init(frame: CGRect) {
